f149fe9a410825d5f7c8b479b41a64169cecd331
arifanf <79965177+arifanf@users.noreply.github.com>
2022-05-09 21:47:26 +0700

    Mengelompokkan

--- src/Components/TodoFooter/__test__/TodoFooter.test.js
+++ src/Components/TodoFooter/__test__/TodoFooter.test.js
@@ -1,18 +1,5 @@
 import { render, screen } from '@testing-library/react'
 
-// [v] 1. Tes container dirender atau tidak
-// 2. Tes kalau props.todoLength > 0 (par.2)
-  // [v] todo-footer-with-items dirender
-  // [v] todo-footer-no-item TIDAK dirender
-// 3. Tes kalau props.todoLength > 1
-  // todo-footer-with-items berisi text "You have {count} tasks"
-// 4. Tes kalau props.todoLength == 1
-  // todo-footer-with-items berisi text "You have 1 task"
-// 5. Tes kalau props.todoLength <= 0
-  // todo-footer-with-items TIDAK dirender
-  // todo-footer-no-item dirender
-
-
 // import component yg akan dites
 import TodoFooter from '../TodoFooter'
 
@@ -27,47 +14,63 @@ test( 'renders todo footer container', () => {
     expect(containerElement).toBeInTheDocument()
 } )
 
+// mengelompokkan
+describe( 'todo length > 0', () => {
+    // todo-footer-with-items dirender
+    test('render "with-items" message', () => {
+        render(<TodoFooter todoLength={5} />)
+        const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
+        expect(footerWithItemsElement).toBeInTheDocument()
+    })
 
+    // todo-footer-no-item TIDAK dirender
+    test('render "with-no-item" message', () => {
+        render(<TodoFooter todoLength={5} />)
+        const footerWithNoItemElement = screen.queryByTestId('todo-footer-no-item')
+        expect(footerWithNoItemElement).not.toBeInTheDocument()
+    })
 
+    describe('todo length == 1', () => {
+        // Tes kalau props.todoLength == 1
+        test('render "TASK" in singular', () => {
+            render(<TodoFooter todoLength={1} />)
+            const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
+            expect(footerWithItemsElement).toHaveTextContent('You have 1 task')
+        })
+    })
 
-// todo-footer-with-items dirender
-test('todo length > 0 - render with item message', () => {
-    render(<TodoFooter todoLength={5} />)
-    const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
-    expect(footerWithItemsElement).toBeInTheDocument()
+    describe('todo length > 1', () => {
+        // Tes kalau props.todoLength > 1
+        test('render "TASK" in plural', () => {
+            render(<TodoFooter todoLength={5} />)
+            const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
+            expect(footerWithItemsElement).toHaveTextContent('You have 5 tasks')
+        })
+    })
+    
 })
 
-// todo-footer-no-item TIDAK dirender
-test('todo length > 0 - render with no item message', () => {
-    render(<TodoFooter todoLength={5} />)
-    const footerWithNoItemElement = screen.queryByTestId('todo-footer-no-item')
-    expect(footerWithNoItemElement).not.toBeInTheDocument()
+
+describe('todo length <=0', () => {
+    // Tes kalau props.todoLength <= 0 : todo-footer-with-items TIDAK dirender
+    test('not render with item message', () => {
+        render(<TodoFooter todoLength={0} />)
+        const footerWithItemsElement = screen.queryByTestId('todo-footer-with-items')
+        expect(footerWithItemsElement).not.toBeInTheDocument()
+    })
+
+    // Tes kalau props.todoLength <= 0 : todo-footer-no-item dirender
+    test('render with no item message', () => {
+        render(<TodoFooter todoLength={0} />)
+        const footerWithNoItemElement = screen.getByTestId('todo-footer-no-item')
+        expect(footerWithNoItemElement).toBeInTheDocument()
+    })
 })
 
-// Tes kalau props.todoLength > 1
-test('todo length > 1, render "TASK" in plural', () => {
-    render(<TodoFooter todoLength={5} />)
-    const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
-    expect(footerWithItemsElement).toHaveTextContent('You have 5 tasks')
-})
 
-// Tes kalau props.todoLength == 1
-test('todo length == 1, render "TASK" in singular', () => {
-    render(<TodoFooter todoLength={1} />)
-    const footerWithItemsElement = screen.getByTestId('todo-footer-with-items')
-    expect(footerWithItemsElement).toHaveTextContent('You have 1 task')
-})
 
-// Tes kalau props.todoLength <= 0 : todo-footer-with-items TIDAK dirender
-test('todo length <= 0 - not render with item message', () => {
-    render(<TodoFooter todoLength={0} />)
-    const footerWithItemsElement = screen.queryByTestId('todo-footer-with-items')
-    expect(footerWithItemsElement).not.toBeInTheDocument()
-})
 
-// Tes kalau props.todoLength <= 0 : todo-footer-no-item dirender
-test('todo length <= 0 - render with no item message', () => {
-    render(<TodoFooter todoLength={0} />)
-    const footerWithNoItemElement = screen.getByTestId('todo-footer-no-item')
-    expect(footerWithNoItemElement).toBeInTheDocument()
-})
+
+
+
+
